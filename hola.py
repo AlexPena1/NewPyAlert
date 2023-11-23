a45210f41e959b841283107d5e9223ee56e5c7ea
Alex Peña <av.5@lasallistas.org.mx>
2023-11-23 11:04:23 -0600

    testj

--- hola.py
+++ hola.py
@@ -25,3 +25,4 @@ with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
     smtp.login(email_sender, email_password)
     smtp.sendmail(email_sender,email_receiver, em.as_string())
 password="ziqn hvrx keus hogd"
+a = 0
